88ffbc967e0b3e98874674b322947602345e7769
jff <>
2019-07-17 15:47:49 +0800

    router 添加

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import IconGlobalStyle from './iconfont/iconfont'
 import Header from './header'
 import {Provider} from 'react-redux'
 import store from './store'
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
 function App() {
   return (
     <Provider store = {store}>
